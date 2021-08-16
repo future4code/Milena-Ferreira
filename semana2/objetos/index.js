682ecb418f3f9d3b562fa6bfadbf8479dd578289
Milena Lara <milenalrf@gmail.com>
2021-08-16 15:43:06 -0300

    Todos os exercícios completos

--- semana2/objetos/index.js
+++ semana2/objetos/index.js
@@ -1,33 +1,33 @@
-// // Exercícios de interpretação de código
+// Exercícios de interpretação de código
 
-// // EXERCÍCIO 1
+// EXERCÍCIO 1
 
-// // Matheus Nachtergaele
-// // Virginia Cavendish
-// // {canal: "Globo", horario: "14h"}
+// Matheus Nachtergaele
+// Virginia Cavendish
+// {canal: "Globo", horario: "14h"}
 
-// // EXERCÍCIO 2
+// EXERCÍCIO 2
 
-// // a)
-// // { nome: "Juca", idade: 3, raca: "SRD" }
-// // { nome: "Juba", idade: 3, raca: "SRD" }
-// // { nome: "Jubo", idade: 3, raca: "SRD" }
+// a)
+// { nome: "Juca", idade: 3, raca: "SRD" }
+// { nome: "Juba", idade: 3, raca: "SRD" }
+// { nome: "Jubo", idade: 3, raca: "SRD" }
 
-// // b) O operador spread (...) copia os dados de um objeto ou array para outro, permitindo que os dados do array original sejam modiicados e outros dados sejam acrescentados no novo array.
+// b) O operador spread (...) copia os dados de um objeto ou array para outro, permitindo que os dados do array original sejam modiicados e outros dados sejam acrescentados no novo array.
 
-// // EXERCÍCIO 3
+// EXERCÍCIO 3
 
-// // a)
-// // false
-// // undefined
+// a)
+// false
+// undefined
 
-// // b) A função acessa o objeto e a propriedade que são passadas como parâmetros e retorna o valor da propriedade em questão. No caso da propriedade "backender", o valor é false. Como a propriedade "altura" não existe no objeto, o valor retornado é undefined.
+// b) A função acessa o objeto e a propriedade que são passadas como parâmetros e retorna o valor da propriedade em questão. No caso da propriedade "backender", o valor é false. Como a propriedade "altura" não existe no objeto, o valor retornado é undefined.
 
-// // --------------------------------------------------
+// --------------------------------------------------
 
-// // Exercícios de escrita de código
+// Exercícios de escrita de código
 
-// // EXERCÍCIO 1
+// EXERCÍCIO 1
 
 // a)
 const pessoa = {
@@ -47,13 +47,92 @@ apresentarSe(novosApelidos);
 const novosApelidos = {...pessoa, apelidos: ["Mica", "Minena", "Nena"]};
 apresentarSe(novosApelidos);
 
-// // EXERCÍCIO 2
+// EXERCÍCIO 2
 
-// // EXERCÍCIO 3
+// a)
+const objeto1 = {
+    nome: "Alice",
+    idade: 23,
+    profissao: "UX designer"
+}
 
+const objeto2 = {
+    nome: "Lucas",
+    idade: 30,
+    profissao: "desenvolvedor web"
+}
 
-// // --------------------------------------------------
+// b)
+function minhaFuncao (objeto) {
+    return [
+            objeto.nome,
+            objeto.nome.length,
+            objeto.idade,
+            objeto.profissao,
+            objeto.profissao.length
+        ] 
+}
 
-// // DESAFIOS
+console.log(minhaFuncao(objeto1));
 
-// // DESAFIO 1
+// EXERCÍCIO 3
+
+// a)
+const carrinho = [];
+
+//b)
+const fruta1 = { nome: "morango", disponibilidade: true};
+const fruta2 = { nome: "pêssego", disponibilidade: true};
+const fruta3 = { nome: "uva", disponibilidade: true};
+
+// c)
+function addCarrinho (fruta) {
+    carrinho.push(fruta);
+}
+// d)
+addCarrinho(fruta1);
+addCarrinho(fruta2);
+addCarrinho(fruta3);
+console.log(carrinho);
+
+// --------------------------------------------------
+
+// DESAFIOS
+
+// DESAFIO 1
+function usuario () {
+    const nome = prompt("Digite seu nome");
+    const idade = Number(prompt("Digite sua idade"));
+    const profissao = prompt("Digite sua profissao");
+    const objeto = {
+        nome: nome,
+        idade: idade,
+        profissao: profissao
+    }
+    console.log(objeto, typeof(objeto));
+}
+usuario();
+
+// DESAFIO 2
+
+const filme1 = {anoLancamento: 2001, nome: "O Fabuloso Destino de Amélie Poulain"};
+const filme2 = {anoLancamento: 2019, nome: "Bacurau"};
+
+function comparaFilmes(filme1, filme2) {
+    console.log("O primeiro filme foi lançado antes do segundo?",
+                filme1.anoLancamento > filme2.anoLancamento);
+    console.log("O primeiro filme foi lançado no mesmo ano do segundo?",
+                filme1.anoLancamento === filme2.anoLancamento);
+}
+comparaFilmes(filme1, filme2);
+
+// DESAFIO 3
+const fruta1 = { nome: "morango", disponibilidade: true};
+const fruta2 = { nome: "pêssego", disponibilidade: true};
+const fruta3 = { nome: "uva", disponibilidade: true};
+
+function controlaEstoque (fruta) {
+    fruta.disponibilidade = !fruta.disponibilidade;
+    return fruta; 
+}
+console.log(controlaEstoque(fruta1));
