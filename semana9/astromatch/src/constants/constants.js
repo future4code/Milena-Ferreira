7cbf72f3cd319f3563ab35112ad55d4f2bb14bd2
Milena Lara <milenalrf@gmail.com>
2021-10-07 12:30:19 -0300

    Recomecei o projeto

--- semana9/astromatch/src/constants/constants.js
+++
@@ -1,9 +0,0 @@
-export const base_url = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch";
-
-export const student = "milena-ferreira-maryam"
-
-export const headers = {
-  headers: {
-    "Content-Type": "application/json"
-  }
-}
