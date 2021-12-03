b616512553a86cf35c59dbe760ff1c1da2b019d3
Milena Lara <milenalrf@gmail.com>
2021-12-03 17:32:31 -0300

    Exercício 1a e 1b

---
+++ semana16/aula47-knex/cinema3.md
@@ -0,0 +1,34 @@
+# Exercício 1
+
+**a. Explique como é a resposta que temos quando usamos o `raw`.**
+
+Recemos um array que contem várias informações do banco e cujo item do index 0 é um outro array contento um objeto com as informações daquele item no BD, configurado no formato chave : valor, sendo a chave o campo no BD.
+
+**b. Faça uma função que busque um ator pelo nome;**
+
+```js
+const getActorByName = async (name: string): Promise<any> => {
+	const result = await connection.raw(`
+		SELECT * FROM Actor WHERE name = "${name}";
+	`);
+
+	return result[0][0];
+}
+```
+
+Endpoint usando a função:
+
+```js
+app.get("/users/:name", async (req: Request, res: Response) => {
+	try {
+		const name = req.params.name;
+
+		console.log(await getActorByName("Juliana Paes"));
+	} catch (error: any) {
+		res.status(500).send("Unexpected error")
+	}
+})
+```
+
+
+**c. Faça uma função que receba um `gender` retorne a quantidade de itens na tabela Actor com esse `gender`. Para atrizes, `female` e para atores `male`.**
--- semana16/aula47-knex/src/index.ts
+++ semana16/aula47-knex/src/index.ts
@@ -14,40 +14,60 @@ app.get("/test", (req, res) => {
 
 const getActorById = async (id: string): Promise<any> => {
 	const result = await connection.raw(`
-    SELECT * FROM Actor WHERE id = '${id}'
+    SELECT * FROM Actor WHERE id = '${id}';
   `)
 
 	return result[0][0]
 }
 
-// Assim a chamada funciona fora dos endpoints com .then()/.catch
-getActorById("001")
-	.then(result => {
-		console.log("log 1", result)
-	})
-	.catch(err => {
-		console.log("log 2", err)
-	});
+const getActorByName = async (name: string): Promise<any> => {
+	const result = await connection.raw(`
+		SELECT * FROM Actor WHERE name = "${name}";
+	`);
 
-// Assim a chamada funciona fora dos endpoints com await
-(async () => {
-	console.log("log 3", await getActorById("001"))
-})()
+	return result[0][0];
+}
 
-
-// Ou então podemos chamá-la dentro de um endpoint
-app.get("/users/:id", async (req: Request, res: Response) => {
+app.get("/users/:name", async (req: Request, res: Response) => {
 	try {
-		const id = req.params.id
+		const name = req.params.name;
 
-		console.log("log 4", await getActorById(id))
-
-		res.end()
+		console.log(await getActorByName("Juliana Paes"));
 	} catch (error: any) {
-		console.log("log ", error.message)
 		res.status(500).send("Unexpected error")
 	}
-}) // bata no http://localhost:3003/users/001 e veja o que acontece no terminal
+})
+
+// // Assim a chamada funciona fora dos endpoints com .then()/.catch
+// getActorById("001")
+// 	.then(result => {
+// 		console.log("log 1", result)
+// 	})
+// 	.catch(err => {
+// 		console.log("log 2", err)
+// 	});
+
+// // Assim a chamada funciona fora dos endpoints com await
+// (async () => {
+// 	console.log("log 3", await getActorById("001"))
+// })()
+
+
+// // Ou então podemos chamá-la dentro de um endpoint
+// app.get("/users/:id", async (req: Request, res: Response) => {
+// 	try {
+// 		const id = req.params.id
+
+// 		console.log("log 4", await getActorById(id))
+
+// 		res.end()
+// 	} catch (error: any) {
+// 		console.log("log ", error.message)
+// 		res.status(500).send("Unexpected error")
+// 	}
+// }) // bata no http://localhost:3003/users/001 e veja o que acontece no terminal
+
+
 
 const server = app.listen(process.env.PORT || 3003, () => {
 	if (server) {
