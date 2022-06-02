23183a02994ae9a352b438496b146fd413fbc083
Milena Lara <milenalrf@gmail.com>
2022-06-02 15:13:58 -0300

    Exercise 3 done

--- node-package-json/index.js
+++ node-package-json/index.js
@@ -13,28 +13,42 @@
 
 // console.log(`Olá, ${name}! Você tem ${age} anos. Em sete anos você terá ${ageInSevenYears} anos.`);
 
-// Exercício 2
+// // Exercício 2
 
-const operation = process.argv[2];
-const firstNumber = Number(process.argv[3]);
-const secondNumber = Number(process.argv[4]);
-let result = 0;
+// const operation = process.argv[2];
+// const firstNumber = Number(process.argv[3]);
+// const secondNumber = Number(process.argv[4]);
+// let result = 0;
 
-switch (operation) {
-    case "add":
-        result = firstNumber + secondNumber;
-        break;
-    case "subtract":
-        result = firstNumber - secondNumber;
-        break;
-    case "multiply":
-        result = firstNumber * secondNumber;
-        break;
-    case "divide":
-        result = firstNumber / secondNumber;
-        break;
-    default:
-        console.log("Error");
-}
+// switch (operation) {
+//     case "add":
+//         result = firstNumber + secondNumber;
+//         break;
+//     case "subtract":
+//         result = firstNumber - secondNumber;
+//         break;
+//     case "multiply":
+//         result = firstNumber * secondNumber;
+//         break;
+//     case "divide":
+//         result = firstNumber / secondNumber;
+//         break;
+//     default:
+//         console.log("Error");
+// }
 
-console.log("Result: " + result);
+// console.log("Result: " + result);
+
+// Exercício 3
+
+const tasks = [
+    "estudar",
+    "comprar pão"
+];
+
+const newTask = process.argv[2];
+tasks.push(newTask);
+
+console.log(`Tarefa adicionada com sucesso!
+
+tarefas: ${tasks}`);
