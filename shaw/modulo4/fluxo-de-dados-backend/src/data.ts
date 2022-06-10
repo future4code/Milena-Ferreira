ac90ff5be979f4ec8fd901a149f2c55ef197273b
Milena Lara <milenalrf@gmail.com>
2022-06-09 23:06:30 -0300

    Exercise 2 done

---
+++ shaw/modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,33 @@
+type produto = {
+  id: string,
+  name: string,
+  price: number
+}
+
+const produtos: Array<produto> = [
+  {
+    id: "1",
+    name: "Pro Longwear Nourishing Foundation",
+    price: 229.90
+  },
+  {
+    id: "2",
+    name: "Máscara de Cílios MAC Stack ",
+    price: 219.00
+  },
+  {
+    id: "3",
+    name: "Batom MAC Love me",
+    price: 109.90
+  },
+  {
+    id: "4",
+    name: "Batom Volumão Quem Disse Berenice",
+    price: 55.90
+  },
+  {
+    id: "5",
+    name: "Lipgloss MAC Stranger Things",
+    price: 129.90
+  },
+]
