6f00ccd0e86d88f85f7c193f5ece0304e07eddf5
Milena Lara <milenalrf@gmail.com>
2022-06-22 11:53:27 -0300

    Refactored exercise 1B - query function is now declared outside of the endpoint

--- shaw/modulo4/knex-js/src/index.ts
+++ shaw/modulo4/knex-js/src/index.ts
@@ -6,19 +6,25 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Funcionou");
 })
 
+const getActorByName = async (name: string): Promise<object[]> => {
+  const result = await connection.raw(`
+    SELECT * FROM Actor WHERE name = "${name}";
+  `);
+
+  return result[0];
+}
+
 app.get("/actor", async (req: Request, res: Response) => {
   let errorCode = 400;
   try {
     const name = req.query.name;
 
-    const result = await connection.raw(`
-        SELECT * FROM Actor WHERE name = "${name}";
-      `);
+    const result = await getActorByName(name as string);
 
-    if (result[0].length === 0) {
+    if (result.length === 0) {
       throw new Error("User not found")
     } else {
-      res.status(200).send(result[0]);
+      res.status(200).send(result);
     }
   } catch (error: any) {
     res.status(errorCode).send(error.sqlMessage || error.message);
