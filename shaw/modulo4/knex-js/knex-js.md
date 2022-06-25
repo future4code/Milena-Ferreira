80a6bde0a607c42f4b144a7318fac9defef636f5
Milena Lara <milenalrf@gmail.com>
2022-06-25 14:26:15 -0300

    Exercise 1C done

--- shaw/modulo4/knex-js/knex-js.md
+++ shaw/modulo4/knex-js/knex-js.md
@@ -6,6 +6,30 @@ a) *Explique como é a resposta que temos quando usamos o `raw`.*
 O raw retornar um array cujo primeiro item é o resultado da query e os demais itens são informações referentes ao banco de dados.
 
 b) *Faça uma função que busque um ator pelo nome;*
+```ts
+const getActorByName = async (name: string): Promise<object[]> => {
+  const result = await connection.raw(`
+    SELECT * FROM Actor WHERE name = "${name}";
+  `);
 
+  return result[0];
+}
+```
 
 c) *Faça uma função que receba um `gender` retorne a quantidade de itens na tabela Actor com esse `gender`. Para atrizes, `female` e para atores `male`.*
+
+```ts
+const countActorsByGender = async (gender: string): Promise<any> => {
+  const result = await connection.raw(`
+    SELECT gender, COUNT(*) as count FROM Actor WHERE gender = "${gender}";
+  `)
+
+  return result[0];
+};
+```
+
+
+
+```ts
+
+```
--- shaw/modulo4/knex-js/src/index.ts
+++ shaw/modulo4/knex-js/src/index.ts
@@ -2,29 +2,46 @@ import { Request, Response } from "express";
 import app from "./app";
 import connection from "./connection";
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Funcionou");
-})
-
 const getActorByName = async (name: string): Promise<object[]> => {
   const result = await connection.raw(`
     SELECT * FROM Actor WHERE name = "${name}";
   `);
 
   return result[0];
-}
+};
+
+const countActorsByGender = async (gender: string): Promise<any> => {
+  const result = await connection.raw(`
+    SELECT gender, COUNT(*) as count FROM Actor WHERE gender = "${gender}";
+  `)
+
+  return result[0];
+};
 
 app.get("/actor", async (req: Request, res: Response) => {
   let errorCode = 400;
   try {
-    const name = req.query.name;
+    const gender: string = req.query.gender as string;
 
-    const result = await getActorByName(name as string);
+    const countActors = await countActorsByGender(gender);
 
-    if (result.length === 0) {
+    res.status(200).send(countActors);
+  } catch (error: any) {
+    res.status(errorCode).send(error.sqlMessage || error.message);
+  }
+});
+
+app.get("/actor/:name", async (req: Request, res: Response) => {
+  let errorCode = 400;
+  try {
+    const name: string = req.params.name;
+
+    const actor = await getActorByName(name as string);
+
+    if (actor.length === 0) {
       throw new Error("User not found")
     } else {
-      res.status(200).send(result);
+      res.status(200).send(actor);
     }
   } catch (error: any) {
     res.status(errorCode).send(error.sqlMessage || error.message);
