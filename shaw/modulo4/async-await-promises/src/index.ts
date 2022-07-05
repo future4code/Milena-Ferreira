1ad3787a6b7b676bcc68f8a698865e9357ccf5a9
Milena Lara <milenalrf@gmail.com>
2022-07-05 14:11:39 -0300

    Exercises 1 and 2 done

--- shaw/modulo4/async-await-promises/src/index.ts
+++ shaw/modulo4/async-await-promises/src/index.ts
@@ -1,1 +1,26 @@
-import app from "./app";
+import axios from "axios";
+import { baseUrl } from "./baseUrl";
+
+// Exercício 1
+async function getAllSubscribers(): Promise<any[]> {
+  const response: Array<any> = await axios.get(`${baseUrl}/news`);
+  return response;
+};
+
+// Exercício 2
+const getAllSubscribersArrow = async (): Promise<any[]> => {
+  const response: Array<any> = await axios.get(`${baseUrl}/news`);
+  return response;
+};
+
+
+const main = async (): Promise<void> => {
+  try {
+    const subscribers = await getAllSubscribers();
+    console.log(subscribers)
+  } catch (error: any) {
+
+  }
+}
+
+main();
