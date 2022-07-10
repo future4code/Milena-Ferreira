cc59d14d094786a8c25892aa134d2bb0a6c5c751
Milena Lara <milenalrf@gmail.com>
2022-07-10 16:58:01 -0300

    working on getPurchase endpoint

--- shaw/modulo4/labecommerce/src/endpoints/getPurchase.ts
+++ shaw/modulo4/labecommerce/src/endpoints/getPurchase.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { Purchase } from "../types/Purchase";
+import selectPurchase from "../data/selectPurchase";
 
 const getPurchase = async (req: Request, res: Response) => {
   let errorCode = 400;
@@ -10,9 +11,9 @@ const getPurchase = async (req: Request, res: Response) => {
       id = "";
     }
 
-    const products: Product[] = await selectProduct(id);
+    const purchases: Purchase[] = await selectPurchase(id);
 
-    res.status(200).send(products);
+    res.status(200).send(purchases);
   } catch (error: any) {
     res.status(errorCode).send(error.sqlMessage || error.message);
   }
